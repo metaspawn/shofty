5a45033418a5eb0d03ff686ba8cc92f2f7fdef99
metaspawn oficial <metaspawn1@hotmail.com>
2026-07-02 15:40:43 -0700

    Refactor catdes.asm for improved structure

--- kernel/catdes.asm
+++ kernel/catdes.asm
@@ -1,23 +1,18 @@
 ; ============================================
-; SHOFTY - Kernel v0.1 + catdes
+; SHOFTY - catdes (cat design)
+; Boot screen: tuxedo cat + loading bar
 ; MetaSpawn Project - GPL-3.0
 ; ============================================
 
-[org 0x1000]
-[bits 16]
-
-kernel_start:
-    mov ax, 0x0003
-    int 0x10
-
-    mov si, catdes
+catdes_show:
+    mov si, cat
     call print
 
     mov si, loading
     call print
 
     mov cx, 15
-.bar:/
+.bar:
     mov al, 219
     mov ah, 0x0E
     mov bh, 0
@@ -32,32 +27,17 @@ kernel_start:
 
     mov si, done
     call print
-
-    jmp $
-
-print:
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
-    jmp .loop
-.done:
     ret
 
-catdes db 13, 10
-       db "      /\_____/\ ", 13, 10
-       db "     /  o   o  \ ", 13, 10
-       db "    ( ==  ^  == )", 13, 10
-       db "     )         ( ", 13, 10
-       db "    (           )", 13, 10
-       db "   ( (  )   (  ) )", 13, 10
-       db "  (__(__)___(__)__)", 13, 10
-       db "      S H O F T Y", 13, 10, 13, 10, 0
+cat db 13, 10
+    db "      /\_____/\ ", 13, 10
+    db "     /  o   o  \ ", 13, 10
+    db "    ( ==  ^  == )", 13, 10
+    db "     )         ( ", 13, 10
+    db "    (           )", 13, 10
+    db "   ( (  )   (  ) )", 13, 10
+    db "  (__(__)___(__)__)", 13, 10
+    db "      S H O F T Y", 13, 10, 13, 10, 0
 
 loading db "   Loading SHOFTY... ", 13, 10, "   [", 0
 done    db "]", 13, 10, 13, 10, "   Welcome.", 13, 10, 0
-
-times 1024-($-$$) db 0
