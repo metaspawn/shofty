ca3361e96a996ccb33467b415c13ace1934ed604
metaspawn oficial <metaspawn1@hotmail.com>
2026-07-03 08:02:13 -0700

    Implement print_string function in kernel_util.asm

--- kernel/kernel_util.asm
+++ kernel/kernel_util.asm
@@ -1,2 +1,11 @@
-%include "kernel/kernel_util.asm"
-%include "kernel/menu.asm"
+; ---------- print_string: prints null-terminated string at SI ----------
+print_string:
+    mov ah, 0x0E
+.next:
+    lodsb               ; al = [si], si++
+    test al, al
+    jz .end
+    int 0x10
+    jmp .next
+.end:
+    ret
