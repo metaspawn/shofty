b37690b772eafd57496cca0e52c30399e0c3af63
metaspawn oficial <metaspawn1@hotmail.com>
2026-07-03 08:13:23 -0700

    Update VGA menu handling and memory allocation

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -11,7 +11,25 @@ kernel_start:
     int 0x10
 
     call catdes_show
-jmp echo_loop
+call vga_menu
+
+    cmp byte [menu_choice], 0
+    je enter_vga
+    jmp $                   ; "no" -> parked for now
+
+enter_vga:
+    mov ah, 0x00
+    mov al, 0x13
+    int 0x10
+
+    mov ax, 0xA000
+    mov es, ax
+    xor di, di
+    mov al, 0x36
+    mov cx, 320*200
+    rep stosb
+
+    jmp $
 print:
 .loop:
     lodsb
@@ -27,4 +45,4 @@ print:
 %include "kernel/catdes.asm"
 %include "kernel/kernel_util.asm"
 
-times 1024-($-$$) db 0
+times 8192-($-$$) db 0
