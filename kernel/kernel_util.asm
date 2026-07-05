903d7433efc243eebd5426ddf77ca874783e42c9
metaspawn oficial <metaspawn1@hotmail.com>
2026-07-05 11:09:08 -0700

    Add print_hex_byte function for hex digit printing
    Implement print_hex_byte function to print a byte as hex digits.

--- kernel/kernel_util.asm
+++ kernel/kernel_util.asm
@@ -9,3 +9,26 @@ print_string:
     jmp .next
 .end:
     ret
+; ---------- print_hex_byte: prints AL as two hex digits ----------
+print_hex_byte:
+    push ax
+    push bx
+    mov bl, al
+    shr al, 4
+    call .digit
+    mov al, bl
+    and al, 0x0F
+    call .digit
+    pop bx
+    pop ax
+    ret
+.digit:
+    and al, 0x0F
+    add al, '0'
+    cmp al, '9'
+    jbe .print
+    add al, 7           ; 'A'-'9'-1
+.print:
+    mov ah, 0x0E
+    int 0x10
+    ret
