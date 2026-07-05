07d442265a21c5ff3c48d8efe6d344a565cf331d
metaspawn oficial <metaspawn1@hotmail.com>
2026-07-05 11:49:47 -0700

    Refactor kernel.asm to remove redundant code
    Removed redundant calls to catdes_show and login_screen, and eliminated the enter_vga section.

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -16,24 +16,9 @@ kernel_start:
     int 0x10
     call catdes_show
     call login_screen
-    call catdes_show
-call login_screen
-call vga_menu
-    cmp byte [menu_choice], 0
-    je enter_vga
-    jmp shell_start                  ; "no" -> parked for now
-enter_vga:
-    mov ah, 0x00
-    mov al, 0x13
-    int 0x10
-
-    mov ax, 0xA000
-    mov es, ax
-    xor di, di
-    mov al, 0x36
-    mov cx, 320*200
-    rep stosb
-
+call catdes_show
+    call login_screen
+    jmp shell_start
     jmp $
 print:
 .loop:
