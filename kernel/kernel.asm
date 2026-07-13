9090d15bfbf68cf3661021ab045f9420a124ca32
metaspawn oficial <metaspawn1@hotmail.com>
2026-07-13 14:57:45 -0700

    Refactor SHOFTY shell implementation

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,40 +1,277 @@
-; ============================================
-; SHOFTY - Kernel v0.1
-; MetaSpawn Project - GPL-3.0
-; ============================================
+; ================= SHOFTY SHELL =================
+; Interactive shell. Needs: kernel_util.asm, login.asm,
+; catdes.asm, disk.asm, sfm.asm.
 
-[org 0x1000]
-[bits 16]
-
-kernel_start:
-kernel_start:
-    mov [boot_drive_k], dl  ; save real boot drive (from bootloader)
-    xor ax, ax
-    mov ds, ax              ; data segment = 0
-    mov es, ax              ; extra segment = 0 (critical for disk I/O!)
-    mov ax, 0x0003
+shell_start:
+    ; clear screen (set video mode resets it)
+    mov ah, 0x00
+    mov al, 0x03        ; 80x25 text mode
     int 0x10
-    call catdes_show
-    call login_screen
+
+    mov si, banner
+    call print_string
+
+shell_loop:
+    ; prompt: shofty (user)>
+    mov si, prompt_open
+    call print_string
+    mov si, current_user
+    call print_string
+    mov si, prompt_close
+    call print_string
+
+    call read_line      ; fills input_buffer, returns on Enter
+
+    ; --- integrity guard: is the filesystem still alive? ---
+    call sfm_guard
+
+    ; empty input? just prompt again
+    mov si, input_buffer
+    cmp byte [si], 0
+    je shell_loop
+
+    ; --- compare against commands ---
+    mov si, input_buffer
+    mov di, cmd_help
+    call str_equals
+    jc .do_help
+
+    mov si, input_buffer
+    mov di, cmd_clear
+    call str_equals
+    jc shell_start      ; clear = redraw screen
+
+    mov si, input_buffer
+    mov di, cmd_cat
+    call str_equals
+    jc .do_cat
+
+    mov si, input_buffer
+    mov di, cmd_vga
+    call str_equals
+    jc .do_vga
+
+    mov si, input_buffer
+    mov di, cmd_disktest
+    call str_equals
+    jc .do_disktest
+
+    mov si, input_buffer
+    mov di, cmd_format
+    call str_equals
+    jc .do_format
+
+    mov si, input_buffer
+    mov di, cmd_chkdsk
+    call str_equals
+    jc .do_chkdsk
+
+    mov si, input_buffer
+    mov di, cmd_panic
+    call str_equals
+    jc .do_panic
+
+    mov si, input_buffer
+    mov di, cmd_warn
+    call str_equals
+    jc .do_warn
+
+    mov si, input_buffer
+    mov di, cmd_corrupt
+    call str_equals
+    jc .do_corrupt
+
+    ; unknown command
+    mov si, msg_unknown
+    call print_string
+    jmp shell_loop
+
+.do_help:
+    mov si, msg_help
+    call print_string
+    jmp shell_loop
+
+.do_cat:
+    mov si, cat         ; reuse the splash cat!
+    call print_string
+    jmp shell_loop
+
+.do_vga:
+    mov ah, 0x00
+    mov al, 0x13        ; VGA 320x200, 256 colors
+    int 0x10
+
+    mov ax, 0xA000
+    mov es, ax
+    xor di, di
+    mov al, 0x36
+    mov cx, 320*200
+    rep stosb
+
+    mov ah, 0           ; wait for key, return to text shell
+    int 0x16
     jmp shell_start
-    jmp $
-print:
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0E
-    mov bh, 0
-    int 0x10
-    jmp .loop
-.done:
-    ret
-%include "kernel/menu.asm"
-%include "kernel/catdes.asm"
-%include "kernel/kernel_util.asm"
-%include "kernel/login.asm"
-%include "kernel/shell.asm"
-%include "kernel/disk.asm"
-%include "kernel/sfm.asm"
 
-times 16384-($-$$) db 0
+.do_disktest:
+    xor ax, ax
+    mov es, ax
+
+    mov ax, 0           ; read test: boot sector
+    mov bx, disk_buf
+    call disk_read
+    jc .dt_fail
+
+    mov si, msg_read_ok
+    call print_string
+
+    mov ax, 20          ; write test: sector 20 round trip
+    mov bx, disk_buf2
+    call disk_write
+    jc .dt_fail
+
+    mov di, disk_buf2
+    mov cx, 512
+    mov al, 0
+    rep stosb
+
+    mov ax, 20
+    mov bx, disk_buf2
+    call disk_read
+    jc .dt_fail
+
+    mov si, disk_buf2
+    call print_string
+    jmp shell_loop
+
+.dt_fail:
+    mov si, msg_dt_fail
+    call print_string
+    mov al, [disk_err]
+    call print_hex_byte
+    mov si, shell_nl
+    call print_string
+    jmp shell_loop
+
+.do_format:
+    xor ax, ax
+    mov es, ax
+    call sfm_format
+    jmp shell_loop
+
+.do_chkdsk:
+    xor ax, ax
+    mov es, ax
+    call sfm_diskcheck
+    jmp shell_loop
+
+.do_panic:
+    mov si, err_ptest
+    jmp panic           ; jmp - there is no coming back
+
+.do_warn:
+    mov si, err_wtest
+    call warning        ; call - it returns
+    jmp shell_loop
+
+.do_corrupt:
+    xor ax, ax
+    mov es, ax
+    call sfm_corrupt
+    jmp shell_loop
+
+; ---------- read_line: reads keys into input_buffer until Enter ----------
+read_line:
+    mov di, input_buffer
+    xor cx, cx
+
+.key:
+    mov ah, 0
+    int 0x16
+
+    cmp al, 0x0D        ; Enter
+    je .done
+
+    cmp al, 0x08        ; Backspace
+    je .backspace
+
+    cmp cx, 63
+    jae .key
+
+    stosb
+    inc cx
+    mov ah, 0x0E
+    int 0x10
+    jmp .key
+
+.backspace:
+    test cx, cx
+    jz .key
+    dec di
+    dec cx
+    mov ah, 0x0E
+    mov al, 0x08
+    int 0x10
+    mov al, ' '
+    int 0x10
+    mov al, 0x08
+    int 0x10
+    jmp .key
+
+.done:
+    mov byte [di], 0
+    mov si, shell_nl
+    call print_string
+    ret
+
+; ---------- str_equals: compares SI vs DI, carry set if equal ----------
+str_equals:
+.loop:
+    mov al, [si]
+    mov bl, [di]
+    cmp al, bl
+    jne .no
+    test al, al
+    jz .yes
+    inc si
+    inc di
+    jmp .loop
+.yes:
+    stc
+    ret
+.no:
+    clc
+    ret
+
+; ---------- shell data ----------
+banner       db "SHOFTY shell v0.2", 13, 10
+             db "Type 'help' for commands.", 13, 10, 13, 10, 0
+prompt_open  db "shofty (", 0
+prompt_close db ")> ", 0
+shell_nl     db 13, 10, 0
+msg_unknown  db "Unknown command. Try 'help'.", 13, 10, 0
+msg_help     db "Commands:", 13, 10
+             db "  help     - show this list", 13, 10
+             db "  clear    - clear the screen", 13, 10
+             db "  cat      - meow", 13, 10
+             db "  vga      - graphics mode (any key returns)", 13, 10
+             db "  disktest - test disk read/write", 13, 10
+             db "  format   - create SFM filesystem on disk", 13, 10
+             db "  chkdsk   - run disk examination", 13, 10, 0
+cmd_help     db "help", 0
+cmd_clear    db "clear", 0
+cmd_cat      db "cat", 0
+cmd_vga      db "vga", 0
+cmd_disktest db "disktest", 0
+cmd_format   db "format", 0
+cmd_chkdsk   db "chkdsk", 0
+cmd_panic    db "panic", 0
+cmd_warn     db "warn", 0
+cmd_corrupt  db "corrupt", 0
+err_ptest    db "manual panic (testing)", 0
+err_wtest    db "test warning: reality is fine, probably", 0
+msg_read_ok  db "read OK!", 13, 10, 0
+msg_dt_fail  db "disk error! code: ", 0
+disk_buf     times 512 db 0
+disk_buf2    db "SFM disk I/O works!", 13, 10, 0
+             times 512-21 db 0
+input_buffer times 64 db 0
